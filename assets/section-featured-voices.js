0186f11056c73a5d1f96196b42859868297b1f85
y.murakami <y.murakami@tech-direction.jp>
2025-08-06 18:12:04 +0900

    swiperが複数ある場合にバグが起きるようになっていたため修正

--- assets/section-featured-voices.js
+++ assets/section-featured-voices.js
@@ -1,33 +1,38 @@
 
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  allowTouchMove: false,
-  speed: 6000,
-  autoplay: {
-    delay: 0,
-  },
-  navigation: false,
-  pagination: false,
-  breakpoints: {
-    1200: {
-      slidesPerView: 4,
-      spaceBetween: 30,
+document.addEventListener('DOMContentLoaded', function () {
+  const featuredVoicesSwiper = document.querySelector('.featured-voices-swiper');
+  if (!featuredVoicesSwiper) return;
+
+  const swiper = new Swiper(featuredVoicesSwiper, {
+    loop: true,
+    allowTouchMove: false,
+    speed: 6000,
+    autoplay: {
+      delay: 0,
     },
-    989: {
-      slidesPerView: 4,
-      spaceBetween: 24,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 16,
-    },
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 8,
-    },
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 8,
+    navigation: false,
+    pagination: false,
+    breakpoints: {
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      989: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 16,
+      },
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 8,
+      },
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 8,
+      }
     }
-  }
+  });
 });
